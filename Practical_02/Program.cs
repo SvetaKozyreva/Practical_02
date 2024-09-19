2e80cf4bbc92391e335eaecc1d89f3c5e0e480d0
SvetaKozyreva <svtkozyreva@gmail.com>
2024-09-19 20:18:01 +0300

    Add project files.

---
+++ Practical_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Practical_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Login:");
+            string login = Console.ReadLine();
+
+            var log = new Regex(@"^[A-Za-z][A-Za-z0-9]{1,9}$");
+
+            if (log.IsMatch(login))
+            {
+                Console.WriteLine($"Логін коректний\nLogin: {login}");
+            }
+            else
+            {
+                Console.WriteLine("Логін некоректний. Він повинен містити від 2 до 10 символів, тільки літери і цифри, і не починатися з цифри.");
+            }
+        }
+    }
+}
